59cabe4c091767069acafddd34bbfbb25bce3a0e
yuliyambg <yuliyambg@gmail.com>
2021-02-07 21:09:56 -0500

    commit tree 2c8c24f26ecacce5b95ec4e43fd422522173bee3

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let main = document.getElementById('main')
+main.remove();
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory';
+newHeader.innerHTML = "Yuliya is the champion";
